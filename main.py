a955ad90e4a0ca30e6140f0d7caa89d69bf6c0aa
Ricardo Arango Giraldo <riarangogi@gmail.com>
2019-02-27 18:33:47 -0500

    I dropped  encoding variable. I want to make a diccionary with ranking state by prop churn sort

--- main.py
+++ main.py
@@ -6,20 +6,17 @@ import matplotlib.pyplot as plt
 data=pd.read_csv("./Data Raw/bigml_59c28831336c6604c800002a.csv")
 data.head()
 
-table=pd.crosstab(data["state"],data["churn"])
+table=pd.crosstab(index=data["state"],columns=data["churn"])
 df_table=pd.DataFrame(table)
+df_table.head()
 
 df_table["total customer"]=df_table[False]+df_table[True]
 df_table["prop customer"]=df_table[True]/df_table["total customer"]
-
 df_table.sort_values(by="prop customer",inplace=True)
-df_table["encoding"]=range(len(df_table))
-df_table.head()
 
-
-df_table=pd.DataFrame(table)
-table_2["encoding"]=range(len(table_2))
-
-state_dic={
-
-}
+states=df_table.index
+for i in len(states):
+    state_dic={"state":
+    {states[i]:i}
+    }
+state_dic
