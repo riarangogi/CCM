cf76d73040c9fc8bd3a0cb983d81bedff7d2d1f9
Ricardo Arango Giraldo <riarangogi@gmail.com>
2019-02-28 16:30:45 -0500

    I going to makethe
    SEMMA metodology, this script is the explorer part

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ data.isnull().sum()
 data.head()
 
 data["churn"].hist()
+ratio_churn=()
 
 table=pd.crosstab(index=data["state"],columns=data["churn"])
 table.plot(kind="bar")
@@ -47,31 +48,5 @@ data["total intl calls"].hist()
 
 data["customer service calls"].hist()
 
-table=pd.crosstab(index=data["state"],columns=data["churn"])
-df_table=pd.DataFrame(table)
-df_table.head()
-
-df_table["total customer"]=df_table[False]+df_table[True]
-df_table["prop customer"]=df_table[True]/df_table["total customer"]
-df_table.sort_values(by="prop customer",inplace=True)
-
-states=df_table.index
-state_dic={'state' : {state: i for state,i in zip(states,range(len(states)))}}
-
-data.replace(state_dic,inplace=True)
-data.head()
-
-data["churn"]=data["churn"].astype(int)
-international_dic={"international plan":{"no":0,"yes":1}}
-data.replace(international_dic,inplace=True)
-mail_dic={"voice mail plan":{"no":0,"yes":1}}
-data.replace(mail_dic,inplace=True)
-data.dtypes
-
 data.drop(["phone number","area code"],axis=1,inplace=True)
 data.head()
-
-y=data["churn"]
-X=data.drop("churn",axis=1)
-
-ratio_churn=()
