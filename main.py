20254fabd7093c2b62ba875810295d78dd176998
Ricardo Arango Giraldo <riarangogi@gmail.com>
2019-02-28 11:06:28 -0500

    I changed all variables to numeric values and I dropped number phone area code columns

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data=pd.read_csv("./Data Raw/bigml_59c28831336c6604c800002a.csv")
+data.dtypes
 data.head()
 
 table=pd.crosstab(index=data["state"],columns=data["churn"])
@@ -15,4 +17,14 @@ states=df_table.index
 state_dic={'state' : {state: i for state,i in zip(states,range(len(states)))}}
 
 data.replace(state_dic,inplace=True)
+data.head()
+
+data["churn"]=data["churn"].astype(int)
+international_dic={"international plan":{"no":0,"yes":1}}
+data.replace(international_dic,inplace=True)
+mail_dic={"voice mail plan":{"no":0,"yes":1}}
+data.replace(mail_dic,inplace=True)
+data.dtypes
+
+data.drop(["phone number","area code"],axis=1,inplace=True)
 data.head()
