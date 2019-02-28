3077beba9000c92d5740f40d9c734505e10d5809
Ricardo Arango Giraldo <riarangogi@gmail.com>
2019-02-28 14:31:42 -0500

    I made the explorer analysis of continues variables

--- main.py
+++ main.py
@@ -17,6 +17,36 @@ table.plot(kind="bar",stacked=True)
 table=pd.crosstab(index=data["voice mail plan"],columns=data["churn"])
 table.plot(kind="bar",stacked=True)
 
+data["account length"].hist()
+
+data["number vmail messages"].hist()
+
+data["total day minutes"].hist()
+
+data["total day charge"].hist()
+
+data["total day calls"].hist()
+
+data["total eve minutes"].hist()
+
+data["total eve charge"].hist()
+
+data["total eve calls"].hist()
+
+data["total night minutes"].hist()
+
+data["total night charge"].hist()
+
+data["total night calls"].hist()
+
+data["total intl minutes"].hist()
+
+data["total intl charge"].hist()
+
+data["total intl calls"].hist()
+
+data["customer service calls"].hist()
+
 table=pd.crosstab(index=data["state"],columns=data["churn"])
 df_table=pd.DataFrame(table)
 df_table.head()
