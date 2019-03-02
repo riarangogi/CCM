cf29c4ab09090dc1ca0b0df9da4e5c8fb94f2e02
Ricardo Arango Giraldo <riarangogi@gmail.com>
2019-03-02 03:49:01 -0500

    I splited dataset in train and test set. I used the train_test_split funtion of sklearn.model_selection packages

--- Modify.py
+++ Modify.py
@@ -1,7 +1,13 @@
 """
 Author: Ricardo Arango Giraldo
+Date: 28/Feb/2019
+Description: In  this script we make all process about modify the dataset for  modeling and analysis
+OS: elementary OS 5.0 Juno (64-bit), Basado en Ubuntu 18.04.2 LTS
+Python Version: Anaconda 3.6.8
 """
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 data=pd.read_csv("./Data Tidy/bigml_tidy.csv")
 data.head()
@@ -21,7 +27,7 @@ data.replace(state_dic,inplace=True)
 data.head()
 
 data["churn"]=data["churn"].astype(int)
-international_dic={"international plan":{"no":0,"yes":1}}
+international_dic={"international plan":{"no":0,"yes":1}}   
 data.replace(international_dic,inplace=True)
 mail_dic={"voice mail plan":{"no":0,"yes":1}}
 data.replace(mail_dic,inplace=True)
@@ -29,3 +35,7 @@ data.dtypes
 
 y=data["churn"]
 X=data.drop("churn",axis=1)
+seed=np.random.seed(47)
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=seed)
+
