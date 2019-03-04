8f3514df8ed38072c66d67b1963b9821a1e3fd97
Ricardo Arango Giraldo <riarangogi@gmail.com>
2019-03-04 16:15:13 -0500

    I made tree classifier. I want to see the variable names in pdf file

--- Modely.py
+++ Modely.py
@@ -6,9 +6,13 @@ OS: elementary OS 5.0 Juno (64-bit), Basado en Ubuntu 18.04.2 LTS
 Python Version: Anaconda 3.6.8
 """
 
-from sklearn.tree import DecisionTreeClassifier
 from  sklearn.metrics import accuracy_score
+from sklearn import tree
+import graphviz as grp
 
-tre_gini=DecisionTreeClassifier(criterion="gini",random_state=47,max_depth=3,min_samples_leaf=5)
-tre_gini.fit(X_train_tree,y_train)
+tree_gini=tree.DecisionTreeClassifier(criterion="gini",random_state=47,max_depth=3,min_samples_leaf=5)
+tree_gini=tree_gini.fit(X_train_tree,y_train)
 
+dot=tree.export_graphviz(tree_gini,out_file=None)
+graph=grp.Source(dot)
+graph.render("Tree")
