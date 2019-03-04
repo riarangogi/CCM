634d05719dce8ed62f008a9d07728abcf4ec96b5
Ricardo Arango Giraldo <riarangogi@gmail.com>
2019-03-04 11:26:52 -0500

    I created the Modely.py

---
+++ Modely.py
@@ -0,0 +1,7 @@
+"""
+Author: Ricardo Arango Giraldo
+Date: 04/Mar/2019
+Description: In  this script we built and developed the classifiers of customers
+OS: elementary OS 5.0 Juno (64-bit), Basado en Ubuntu 18.04.2 LTS
+Python Version: Anaconda 3.6.8
+"""
