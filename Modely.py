35020fb51f46c8ecfca057eaf02040f0f47bb93b
Ricardo Arango Giraldo <riarangogi@gmail.com>
2019-03-04 14:40:57 -0500

    I developed a tree classifier with X_train_tree data

--- Modely.py
+++ Modely.py
@@ -5,3 +5,10 @@ Description: In  this script we built and developed the classifiers of customers
 OS: elementary OS 5.0 Juno (64-bit), Basado en Ubuntu 18.04.2 LTS
 Python Version: Anaconda 3.6.8
 """
+
+from sklearn.tree import DecisionTreeClassifier
+from  sklearn.metrics import accuracy_score
+
+tre_gini=DecisionTreeClassifier(criterion="gini",random_state=47,max_depth=3,min_samples_leaf=5)
+tre_gini.fit(X_train_tree,y_train)
+
