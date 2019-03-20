58405056eb132042e13945f65add76ad0a2f6d64
Ricardo Arango Giraldo <riarangogi@gmail.com>
2019-03-19 23:09:51 -0500

    I created the Assess.py file

---
+++ Assess.py
@@ -0,0 +1,7 @@
+"""
+Author: Ricardo Arango Giraldo
+Date: 11/Mar/2019
+Description: In  this script we select the classifiers of customers models
+OS: elementary OS 5.0 Juno (64-bit), Basado en Ubuntu 18.04.2 LTS
+Python Version: Anaconda 3.6.8
+"""
